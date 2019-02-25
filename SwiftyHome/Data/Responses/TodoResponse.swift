63fd92586626bcba3b7e795ecc5329c87f5c8972
YutoMizutani <yuto.mizutani.dev@gmail.com>
2019-02-26 00:58:33 +0900

    :bug: Fix JSON decoding #4
    Golangってempty slice がnull JSONになるみたい。https://github.com/golang/go/issues/26866

--- SwiftyHome/Data/Responses/TodoResponse.swift
+++ SwiftyHome/Data/Responses/TodoResponse.swift
@@ -12,15 +12,23 @@ struct TodoResponse: Codable, EntityTranslatable {
     var id: Int64
     var title: String
     var description: String
-    var tags: [String]
+    var tags: [String]?
     var isDone: Bool
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "ID"
+        case title
+        case description
+        case tags
+        case isDone = "is_done"
+    }
 
     typealias EntityType = TodoEntity
     func toEntity() -> EntityType {
         return TodoEntity(id: PrimitiveID(id),
                           title: title,
                           description: description,
-                          tags: tags,
+                          tags: tags ?? [],
                           state: TodoState(isDone))
     }
 }
